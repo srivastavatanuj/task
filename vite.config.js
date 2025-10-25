1f6b66d95fe63760bf62f355d98efda0b0ff1a22
Tanuj <73632086+srivastavatanuj@users.noreply.github.com>
2025-10-25 23:10:18 +0530

    Update and rename home.jsx to Home.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.PNG','**/*.JPG','**/*.JPEG']
-})
+});
